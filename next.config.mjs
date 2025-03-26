e82f0eb39562fba3aa7cd1d3e36b5e8a64a261a9
Riya-Sharma12 <sharmariya0163@gmail.com>
2025-03-26 12:33:45 +0530

    next js file changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
 const nextConfig = {
     images: {
         remotePatterns: [
@@ -8,6 +12,14 @@ const nextConfig = {
           },
         ],
       },
+      webpack: (config) => {
+        config.resolve.alias = {
+         ...config.resolve.alias,
+        '@': path.resolve(__dirname, './'),
+         };
+        return config;
+        },
+
 };
 
 export default nextConfig;
